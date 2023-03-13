4226728474e1363bd563b30a4d8fe506f144e442
Pudge1996 <448404152@qq.com>
2023-03-13 21:27:28 +0800

    组件改进

--- pages/blog/components/common/DownloadFile.jsx
+++ pages/blog/components/common/DownloadFile.jsx
@@ -1,12 +1,9 @@
-import React from "react";
-import Link from "next/link";
-
 const DownloadFile = ({ url, fileName, fileSize }) => {
   return (
     <div className="flex justify-between items-center gap-2 text-lg p-4 rounded-lg ring-default border border-neutral-200 dark:border-neutral-800 transition-[border-color]">
-      <Link className="cursor-pointer link-color ring-default leading-tight" href={url} target="_blank" rel="noopener noreferrer">
+      <a className="cursor-pointer link-color ring-default leading-tight" href={url} target="_blank" rel="noopener noreferrer">
         {fileName}
-      </Link>
+      </a>
       <div className="text-base text-tertiary whitespace-nowrap">{fileSize}</div>
     </div>
   );
