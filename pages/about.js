7ddaef1e5df7f00fc1316ba4b04f1f854bc46b48
Pudge1996 <448404152@qq.com>
2023-05-27 21:29:21 +0800

    更新信息

--- pages/about.js
+++ pages/about.js
@@ -24,14 +24,14 @@ export default function about() {
           LRD.IM 是我的个人网站，记录我的设计作品、博客等，是我设计师生涯中很重要的<span className="whitespace-nowrap">一部分。</span>
           </p>
           <p className="mt-2 sm:mt-3 mb-3">
-          自&thinsp;2017&thinsp;年&thinsp;12&thinsp;月&thinsp;31&thinsp;日建站至今，已经经历了&thinsp;3&thinsp;次改版重构，现在你所看到的网站是使用 <Link href="https://nextjs.org/" target="_blank" rel="noopener noreferrer" title="Next.js">Next.js</Link> 和 <Link href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer" title="Tailwind CSS">Tailwind CSS</Link> 搭建<span className="whitespace-nowrap">而成。</span>
+          自&thinsp;2017&thinsp;年&thinsp;12&thinsp;月&thinsp;31&thinsp;日建站至今，已经经历了&thinsp;3&thinsp;次 “大型” 改版重构，现在你所看到的网站是使用 <Link href="https://nextjs.org/" target="_blank" rel="noopener noreferrer" title="Next.js">Next.js</Link> 和 <Link href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer" title="Tailwind CSS">Tailwind CSS</Link> 搭建<span className="whitespace-nowrap">而成。</span>
           </p>
           
         </section>
         <section>
           <h2 className="text-[22px] font-semibold">其他</h2>
           <p className="mt-2 mb-3">
-          在 <Link href="https://github.com/Pudge1996/NEXT.LRD.IM" target="_blank" rel="noopener noreferrer" title="Github">Github</Link> 中查看本站代码，在 <Link href="/updates" title="Github">What's New</Link> 中查看更新<span className="whitespace-nowrap">记录。</span>
+          本站永久开源，可在 <Link href="https://github.com/Pudge1996/NEXT.LRD.IM" target="_blank" rel="noopener noreferrer" title="Github">Github</Link> 中查看本站代码。期望有大佬在前端体验、代码质量上指点一二。同时也可以在 <Link href="/updates" title="Github">What's New</Link> 中查看更新<span className="whitespace-nowrap">记录。</span>
           </p>
         </section>
       </div>
