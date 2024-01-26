80faf6566d0bcdc3ab2573dbda3cffabfad0a73f
Pudge1996 <448404152@qq.com>
2024-01-26 11:41:55 +0800

    header

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
     title: '李瑞东 LRD.IM',
     author: '李瑞东',
     authorImg: 'https://lrdim.oss-cn-shenzhen.aliyuncs.com/avatar.jpeg',
-    description: '李瑞东的个人网站，记录我在设计路上的作品、经验和观点。',
+    description: '李瑞东的个人网站，记录设计生涯里的作品、经验和观点。',
     theme: 'system', // system, dark or light
     lang: 'zh-Hans-CN',
 }
--- pages/_app.js
+++ pages/_app.js
@@ -14,9 +14,14 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="description" content={siteMetadata.description} />
 
         {/* For Soical Meida (openGraph) */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={siteMetadata.title} />
+        <meta property="og:description" content={siteMetadata.description} />
         <meta property="og:image" content="网站宽屏图（16:9）" />
         <meta property="og:image:alt" content="网站宽屏图的描述" />
-        <meta name="og:type" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={siteMetadata.title} />
+        <meta name="twitter:description" content={siteMetadata.description} />
         <meta name="twitter:site" content="@Pudge_1996" />
         <meta name="twitter:creator" content="@Pudge_1996" />
       </Head>
