6dff1afdda406a417efc73e76ceff60204caf027
Pudge1996 <448404152@qq.com>
2024-02-02 12:07:52 +0800

    ScrollToTopButton supported

--- components/common/ScrollToTop.js
+++ components/common/ScrollToTop.js
@@ -1,7 +1,9 @@
 import { IoArrowUp } from "react-icons/io5"; //https://react-icons.github.io/react-icons/icons?name=io5
 import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function ScrollToTopButton() {
+  const { t } = useTranslation("components");
   const isBrowser = () => typeof window !== "undefined"; //The approach recommended by Next.js
 
   function scrollToTop() {
@@ -30,8 +32,8 @@ export default function ScrollToTopButton() {
         onClick={scrollToTop}
         className={` fixed ${buttonClass} right-4 bottom-4 ring-default sm:hover:text-primary active:text-primary btn-base btn-icon text-tertiary bg-neutral-100 dark:bg-neutral-800 transition-colors `}
       >
-        <span className="sr-only">回到顶部</span>
-        <IoArrowUp className="text-xl" title="回到顶部" />
+        <span className="sr-only">{t("ScrollToTop.button_aria")}</span>
+        <IoArrowUp className="text-xl" aria-label={t("ScrollToTop.button_aria")} title={t("ScrollToTop.button_aria")} />
       </button>
     </>
   );
