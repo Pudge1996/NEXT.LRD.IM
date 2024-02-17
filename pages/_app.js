b0de11647e0b21da66f637c05a7c1cbde8e6ec31
Pudge1996 <448404152@qq.com>
2024-02-17 21:55:25 +0800

    i18N: _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,33 +2,56 @@ import "/styles/globals.css";
 import React from "react";
 import Layout from "/components/layout";
 import Head from "next/head";
-import siteMetadata from "/data/siteMetadata";
 import { ThemeProvider } from "next-themes";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import { Analytics } from '@vercel/analytics/react';
 import useLanguageSetting from '/utils/useLanguageSetting';
-import { appWithTranslation } from 'next-i18next'
+import { appWithTranslation, useTranslation } from 'next-i18next'
 import nextI18NextConfig from '../next-i18next.config'
+import withServerSideTranslations from '/utils/withServerSideTranslations';
+
+export const getServerSideProps = withServerSideTranslations(['common']);
 
 function MyApp({ Component, pageProps }) {
   useLanguageSetting();
+  const { t } = useTranslation('common');
   return (
     <>
       <Head>
-        <meta name="author" content={siteMetadata.author} />
-        <meta name="description" content={siteMetadata.description} />
+        <meta name="author" content={t("common.information.author")} />
+        <meta name="description" content={t("common.information.desc")} />
 
-        {/* For Soical Meida (openGraph) */}
-        <meta property="og:image" content="网站宽屏图（16:9）" />
-        <meta property="og:image:alt" content="网站宽屏图的描述" />
-        <meta name="og:type" content="summary" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={t("common.information.site")} />
+        <meta
+          property="og:description"
+          content={t("common.information.desc")}
+        />
+        <meta
+          property="og:image"
+          content="https://lrdim.oss-accelerate.aliyuncs.com/resources/og_image.png"
+        />
+        <meta
+          property="og:image:alt"
+          content="Capturing the works, experiences, and insights from my design journey."
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={t("common.information.site")} />
+        <meta
+          name="twitter:description"
+          content={t("common.information.desc")}
+        />
+        <meta
+          name="twitter:image"
+          content="https://lrdim.oss-accelerate.aliyuncs.com/resources/og_image.png"
+        />
         <meta name="twitter:site" content="@Pudge_1996" />
         <meta name="twitter:creator" content="@Pudge_1996" />
       </Head>
       <GoogleAnalytics trackPageViews={{ ignoreHashChange: true }} />
       <ThemeProvider attribute="class" storageKey="theme">
         <Layout>
-          <Component {...pageProps}/>
+          <Component {...pageProps} />
           <Analytics />
         </Layout>
       </ThemeProvider>
