c81843f2a4216162dbdf94b778318cd3519498ac
Pudge1996 <448404152@qq.com>
2024-02-17 21:19:56 +0800

    i18N: Updates

--- data/blog/blogCategoriesData.js
+++ data/blog/blogCategoriesData.js
@@ -1,121 +1,43 @@
 // 博客分类数据源
 
-const blogCategoriesData = {
-  "zh-Hans": [
-    {
-      name: "字体 & 排版策略",
-      desc: "我对界面内文字的要求相当严苛。该分类包含了我在 Web 上的字体特点研究，以及文字排版处理技巧等。随着探索的深入，一些设计策略甚至成为了我的「教条」。",
-      url: "typographic",
-      num: "7",
-    },
-    {
-      name: "实践经验沉淀",
-      desc: "工作中的一些实践经验，包括了探索过程和结论。通常是一些有设计机会点的内容，或者踩过坑的案例，充满了血与泪。",
-      url: "experience",
-      num: "7",
-    },
-    {
-      name: "分享会文稿",
-      desc: "在公司的分享会我通常会准备一份文稿，正好可以放到我的博客上面。（不包含早期没有准备文稿的分享会）",
-      url: "sharing",
-      num: "5",
-    },
-    {
-      name: "翻译好文",
-      desc: "为了学习到外部先进的实践经验，我尝试翻译文章来让自己精读某篇文章。主要从工作中竞品公司的 UX 博客来尝试入门。",
-      url: "translate",
-      num: "5",
-    },
-    {
-      name: "软件插件",
-      desc: "生涯早期比较喜欢研究各种软件和插件，所以了记录了不少相关技巧。后面发现掌握设计的方法/策略比掌握某款软件技巧更重要，所以基本不会更新了。",
-      url: "software",
-      num: "13",
-    },
-    {
-      name: "设计之外",
-      desc: "留一点空间记录设计之外的内容，比如本站的建设，或者其他奇思妙想（如有...）",
-      url: "others",
-      num: "6",
-    },
-  ],
-  "zh-Hant": [
-    {
-      name: "字體與排版策略",
-      desc: "我對介面內文字的要求相當嚴苛。該分類包含了我在 Web 上的字體特點研究，以及文字排版處理技巧等。隨著探索的深入，一些設計策略甚至成為了我的「教條」。",
-      url: "typographic",
-      num: "7",
-    },
-    {
-      name: "實務經驗總結",
-      desc: "工作中的一些實踐經驗，包括了探索過程和結論。通常是一些有設計機會點的內容，或者踩過坑的案例，充滿了血與淚。",
-      url: "experience",
-      num: "7",
-    },
-    {
-      name: "分享會檔案",
-      desc: "在公司的分享會我通常會準備一份文稿，正好可以放到我的部落格上面。（不包含早期沒有準備文稿的分享會）",
-      url: "sharing",
-      num: "5",
-    },
-    {
-      name: "翻譯好文",
-      desc: "為了學習到外部先進的實踐經驗，我嘗試翻譯文章來讓自己精讀某篇文章。主要從工作中競品公司的 UX 部落格來嘗試入門。",
-      url: "translate",
-      num: "5",
-    },
-    {
-      name: "軟體與插件",
-      desc: "生涯早期比較喜歡研究各種軟體和插件，所以記錄了不少相關技巧。後面發現掌握設計的方法/策略比掌握某款軟體技巧更重要，所以基本不會更新了。",
-      url: "software",
-      num: "13",
-    },
-    {
-      name: "設計之外",
-      desc: "留一點空間記錄設計之外的內容，比如本站的建設，或者其他奇思妙想（如有...）",
-      url: "others",
-      num: "6",
-    },
-  ],
-  "en": [
-    {
-      name: "Typography & Layout Strategies",
-      desc: "I have stringent requirements for text within interfaces. This category includes my research on web typography features and text layout techniques. As my exploration deepened, some design strategies even became my 'dogmas.'",
-      url: "typographic",
-      num: "7",
-    },
-    {
-      name: "Practical Experiences",
-      desc: "Real-world experiences from work, including explorations and conclusions. Often involves design opportunities or lessons learned from pitfalls, filled with trials and tribulations.",
-      url: "experience",
-      num: "7",
-    },
-    {
-      name: "Design Sharing",
-      desc: "I usually prepare a presentation for design sharing, which fits perfectly on my blog. (Excludes earlier sharing without prepared presentations.)",
-      url: "sharing",
-      num: "5",
-    },
-    {
-      name: "Translated Articles",
-      desc: "To learn from advanced practices, I've tried translating articles for in-depth reading. Mainly, I start with UX blogs from competitor companies at work.",
-      url: "translate",
-      num: "5",
-    },
-    {
-      name: "Software and Plugins",
-      desc: "Early in my career, I was keen on exploring various software and plugins, so I recorded many related tips. Later, I realized mastering design methods/strategies is more important than software skills, so I barely update this anymore.",
-      url: "software",
-      num: "13",
-    },
-    {
-      name: "Beyond Design",
-      desc: "Leaving some space for content beyond design, like the development of this site, or other creative ideas (if any).",
-      url: "others",
-      num: "6",
-    },
-  ],
-}
+const blogCategoriesData = [
+  {
+    name: "字体 & 排版策略",
+    desc: "我对界面内文字的要求相当严苛。该分类包含了我在 Web 上的字体特点研究，以及文字排版处理技巧等。随着探索的深入，一些设计策略甚至成为了我的「教条」。",
+    url: "typographic",
+    num: "7",
+  },
+  {
+    name: "实践经验沉淀",
+    desc: "工作中的一些实践经验，包括了探索过程和结论。通常是一些有设计机会点的内容，或者踩过坑的案例，充满了血与泪。",
+    url: "experience",
+    num: "7",
+  },
+  {
+    name: "分享会文稿",
+    desc: "在公司的分享会我通常会准备一份文稿，正好可以放到我的博客上面。（不包含早期没有准备文稿的分享会）",
+    url: "sharing",
+    num: "5",
+  },
+  {
+    name: "翻译好文",
+    desc: "为了学习到外部先进的实践经验，我尝试翻译文章来让自己精读某篇文章。主要从工作中竞品公司的 UX 博客来尝试入门。",
+    url: "translate",
+    num: "5",
+  },
+  {
+    name: "软件插件",
+    desc: "生涯早期比较喜欢研究各种软件和插件，所以了记录了不少相关技巧。后面发现掌握设计的方法/策略比掌握某款软件技巧更重要，所以基本不会更新了。",
+    url: "software",
+    num: "13",
+  },
+  {
+    name: "设计之外",
+    desc: "留一点空间记录设计之外的内容，比如本站的建设，或者其他奇思妙想（如有...）",
+    url: "others",
+    num: "6",
+  },
+]
   
   export default blogCategoriesData;
   
--- pages/updates.js
+++ pages/updates.js
@@ -2,58 +2,48 @@ import React, { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import siteMetadata from "/data/siteMetadata";
 import updatesData from "/data/common/updatesData";
 import ContactCard from "/components/common/ContactCard";
+import { useTranslation } from 'next-i18next'
+import withServerSideTranslations from '/utils/withServerSideTranslations';
+
+export const getServerSideProps = withServerSideTranslations(['common', "pages"]);
 
 export default function updates() {
+  const { t } = useTranslation(["common", "pages"]);
+  const { i18n } = useTranslation();
+  const dataForCurrentLanguage = updatesData[i18n.language] || updatesData["en"];
   const [isLoaded, setIsLoaded] = useState(false);
   return <>
     <Head>
-      <title>What's New - {siteMetadata.title}</title>
-      <meta name="author" content={siteMetadata.author} />
-      <meta name="description" content={siteMetadata.description} />
-      <link rel="icon" href="/favicon.ico" />
-
-      {/* For Soical Meida (OpenGraph) */}
-      <meta property="og:image" content="网站宽屏图（16:9）" />
-      <meta property="og:image:alt" content="网站宽屏图的描述" />
-      <meta name="og:type" content="summary" />
-      <meta property="og:title" content={siteMetadata.title} />
-      <meta property="og:description" content={siteMetadata.description} />
-
-      {/* For Twitter */}
-      <meta name="twitter:site" content="@Pudge_1996" />
-      <meta name="twitter:creator" content="@Pudge_1996" />
+      <title>{t("common.pages.updates", { ns: "common" })} - {t("common.information.pageTitleSuffix", { ns: "common" })}</title>
     </Head>
 
     <div className="layout updates">
-      <h1 className="">What's New</h1>
+      <h1 className="">{t("common.pages.updates", { ns: "common" })}</h1>
       <p className="mt-3 sm:mt-6 my-1">
-        了解 LRD.IM 的新变化，以及发展历程。
+      {t("updates.desc", { ns: "pages" })}
       </p>
       <p className="flex mt-2 mb-0">
         <Link
           href="https://github.com/Pudge1996/NEXT.LRD.IM/issues"
           target="_blank"
           rel="noopener noreferrer"
-          title="Github Issues"
+          title={t("updates.reportIssues_title", { ns: "pages" })}
         >
-          反馈 Issues
+          {t("updates.reportIssues", { ns: "pages" })}
         </Link>
         <span>&nbsp;&nbsp;·&nbsp;&nbsp;</span>
         <span
-          href="/updates"
-          title="联系方式"
           className="relative link-color no-underline inline-block"
         >
           <ContactCard />
-          与我联系
+          {t("updates.contact", { ns: "pages" })}
         </span>
       </p>
       
       <section className="flex flex-col gap-8 md:gap-20 mt-8 md:mt-14 pl-7 relative before:absolute before:w-[2px] before:bg-neutral-200 dark:before:bg-neutral-800 before:h-[calc(100%-12px)] before:left-2 before:mt-3 before:transition-all ">
-        {updatesData.map((updates) => (
+        {dataForCurrentLanguage.map((updates) => (
           <div className="flex gap-2 md:gap-4 flex-col md:flex-row">
             <div className=" min-w-[12rem] md:sticky md:top-[60px] pb-1 md:pb-0 h-min">
               <time className="text-tertiary relative before:absolute before:w-4 before:h-4 before:bg-neutral-700 dark:before:bg-neutral-200 before:left-[-27px] before:top-[1.5px] before:rounded-full before:border-4 before:border-neutral-200 dark:before:border-neutral-800 before:shadow-[0_0_0_6px_rgba(255,255,255,1)] dark:before:shadow-[0_0_0_6px_rgba(0,0,0,1)] before:transition-all font-medium">
@@ -82,7 +72,7 @@ export default function updates() {
                 </div>
               )}
               <div className="flex flex-col gap-2">
-                <p className="my-0">{updates.desc}</p>
+                <p className="my-0 leading-normal">{updates.desc}</p>
                 {updates.linkURL && (
                   <Link
                     href={updates.linkURL}
