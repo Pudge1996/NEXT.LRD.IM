18467e3460328e1b898b999912dce0208013f1f7
Pudge1996 <448404152@qq.com>
2024-02-24 14:27:15 +0800

    Min

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,18 @@
 import "/styles/globals.css";
 import React from "react";
 import Layout from "/components/layout";
-import { ThemeProvider } from "next-themes";
-import { GoogleAnalytics } from "nextjs-google-analytics";
-import { Analytics } from '@vercel/analytics/react';
 import useLanguageSetting from '/utils/useLanguageSetting';
-import { appWithTranslation, useTranslation } from 'next-i18next'
+import { appWithTranslation } from 'next-i18next'
 import nextI18NextConfig from '../next-i18next.config'
-import withServerSideTranslations from '/utils/withServerSideTranslations';
 
-export const getServerSideProps = withServerSideTranslations(["common", "components", "pages"]);
 
 function MyApp({ Component, pageProps }) {
   useLanguageSetting();
-  const { t } = useTranslation('common');
   return (
     <>
-      <GoogleAnalytics trackPageViews={{ ignoreHashChange: true }} />
-      <ThemeProvider attribute="class" storageKey="theme">
         <Layout>
           <Component {...pageProps} />
-          <Analytics />
         </Layout>
-      </ThemeProvider>
     </>
   );
 }
